8d769b0b6abe137e9923ecb767373daef66064ec
Jiří Pinkava <j-pi@seznam.cz>
2015-11-09 10:20:50 +0100

    LP analysis - autocorrelation

--- rp_celp.py
+++ rp_celp.py
@@ -4,61 +4,26 @@ import numpy as np
 class Codec:
     samp_rate = 8000
 
-    band_expansion = np.array(
-            (1., 0.9995, 0.9982, 0.9959, 0.9928, 0.9888,
-                0.9839, 0.9781, 0.9716, 0.9641, 0.9559, ) )
+    band_expansion = np.array((1., 0.9995, 0.9982, 0.9959, 0.9928, 0.9888,
+        0.9839, 0.9781, 0.9716, 0.9641, 0.9559, ) )
 
     def __init__(self):
         pass
 
-    def encode(self, data):
+    def encode(self, samples):
         """Takes 160 samples in 13 bit uniform format (16 bit signed int)
         and compress it using RP-CELP codec."""
-        lp = self.lp_analysis(data)
+# normalize samples to range <-1, 1>
+        samples = np.array(samples)/(2**12-1)
+        lp = self.lp_analysis(samples)
+        return lp
 
-    def lp_analysis(self, data):
-        data = np.array(data, dtype=np.int16)
-        autocorr = self.autocorrelate(data)
+    def lp_analysis(self, samples):
+        autocorr = self.autocorrelate(samples)
         autocorr = autocorr * Codec.band_expansion
-
-# reflection coefficients, e1 and k0 are index offsets to element with index 0
-# e[0][e1] equals in math script e(0, 0)
-        p = len(autocorr) - 1
-        e1 = p - 1
-        e = np.empty( (p, 2*p,) )
-        for i in range(p):
-            e[0][e1+i] = autocorr[i]
-            e[0][e1-i] = autocorr[i]
-        e[0][e1 + p] = autocorr[p]
-
-        refl_coef = np.empty(p)
-        k0 = - 1
-        for i in range(1, p+1):
-            refl_coef[k0 + i] = (-e[i - 1][e1 + i])/e[i - 1][e1]
-            if i == 10:
-                break
-            for j in range(-p+1+i, p+1):
-                e[i][e1 + j] = e[i-1][e1 + j] + refl_coef[k0 + i] * e[i-1][e1 + i - j]
-
-        lars = []
-        for i in range(len(refl_coef)):
-            rc = refl_coef[i]
-            arc = abs(rc)
-            if arc < 0.675:
-                lar = rc
-            elif arc < 0.950:
-                lar = np.copysign(2*arc - 0.675, rc)
-            else:
-                lar = np.copysign(8*arc - 6.375, rc)
-            lars.append(lar)
-
-        return lars
-
-    def autocorrelate(self, data):
-        # FIXME: should we autocorrelate using previous sample?
-        data10 = np.concatenate((data, np.zeros(10, dtype=np.int16), ))
-        autocorr = []
-        for i in range(11):
-            autocorr.append(np.corrcoef(data, data10[i:i+160])[1][0])
         return autocorr
+
+    def autocorrelate(self, samples):
+        # TODO: should we autocorrelate using samples from previous frame?
+        return [ np.correlate(samples, samples[i:])[0] for i in range(11) ]
 
--- test_rp_celp.py
+++ test_rp_celp.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from numpy.testing import assert_almost_equal
 from rp_celp import Codec
 import numpy as np
 import unittest
@@ -7,12 +8,26 @@ import unittest
 
 class TestCodec(unittest.TestCase):
 # sine at 440 Hz for sample rate 8000 samp/sec
-    sin440 = np.array( [ np.sin(x*2*np.pi/Codec.samp_rate*440) * (2**12-1)
-        for x in range(160) ], dtype=np.int16)
+    sin440 = np.sin(np.linspace(0, 159*2*np.pi/Codec.samp_rate*440, num=160)) * (2**12-1)
+    silence = np.array([0., ] * 160)
+    silence2 = np.array([0.2 * (2**12-1), ] * 160)
 
-    def test_coder(self):
+    def test_autocorrelate(self):
         codec = Codec()
-        codec.encode(TestCodec.sin440)
+
+        r = codec.autocorrelate(TestCodec.sin440/(2**12-1))
+        np.testing.assert_almost_equal(r,
+                [ 79.9559, 74.7536, 60.8274, 39.9249, 14.5934, -12.1309,
+                    -37.0863, -57.3598, -70.6272, -75.4191, -71.2828],
+                decimal=3)
+
+        r = codec.autocorrelate(TestCodec.silence/(2**12-1))
+        assert_almost_equal([0., ] * 11, r, decimal=3)
+
+        r = codec.autocorrelate(TestCodec.silence2/(2**12-1))
+        assert_almost_equal(r,
+                [ 6.4, 6.36, 6.32, 6.28, 6.24, 6.2, 6.16, 6.12, 6.08, 6.04, 6.],
+                decimal=3)
 
 
 if __name__ == '__main__':
