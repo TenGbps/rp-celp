45741cfe4e8d1489792655ae0246893f2b1a09b4
Jiří Pinkava <j-pi@seznam.cz>
2015-11-09 14:38:08 +0100

    LP analysis - reflection coefficients

--- rp_celp.py
+++ rp_celp.py
@@ -21,9 +21,38 @@ class Codec:
     def lp_analysis(self, samples):
         autocorr = self.autocorrelate(samples)
         autocorr = autocorr * Codec.band_expansion
-        return autocorr
+        refl_coefs = self.autocorr2refl_coeffs(autocorr)
+        return refl_coefs
 
     def autocorrelate(self, samples):
         # TODO: should we autocorrelate using samples from previous frame?
         return [ np.correlate(samples, samples[i:])[0] for i in range(11) ]
+
+    def autocorr2refl_coeffs(self, autocorr):
+        """Literature: Speech Coding Algorithms, Wai C. Chu page 119."""
+# prevent divission by zero bellow
+        if abs(autocorr[0]) < 2**-12:
+            autocorr[0] = 2**-12
+
+# e1 and r0 are index offsets to element with math index 0
+# example: math e(0, -1) => python e[0][e1 - 1]
+        M = len(autocorr) - 1
+        e1 = M - 1
+        e = np.empty( (M, 2*M,) )
+# e[0] is simetricaly filled around element 0 with autocorr
+        e[0][e1:] = autocorr
+        e[0][:e1] = autocorr[-2:0:-1]
+
+        refl_coefs = np.empty(M)
+        r0 = - 1
+        for l in range(1, M+1):
+            refl_coefs[r0+l] = (-e[l-1][e1+l])/e[l-1][e1]
+# TODO: clip values in range, in some cases they go a bit out of bounds (check!)
+            refl_coefs[r0+l] = np.clip(refl_coefs[r0+l], -0.99999, 0.99999)
+            if l == M:
+                break
+            for k in range(-M+1+l, M+1):
+                e[l][e1+k] = e[l-1][e1+k] - refl_coefs[r0+l] * e[l-1][e1+l-k]
+
+        return refl_coefs
 
--- test_rp_celp.py
+++ test_rp_celp.py
@@ -29,6 +29,40 @@ class TestCodec(unittest.TestCase):
                 [ 6.4, 6.36, 6.32, 6.28, 6.24, 6.2, 6.16, 6.12, 6.08, 6.04, 6.],
                 decimal=3)
 
+    def test_autocorr2refl_coeffs(self):
+        codec = Codec()
+
+# this one produced results out of expected range <-1., 1>
+        autocorr = np.array((0.34040, 0.29978, 0.25687, 0.21150, 0.18464,
+            0.21603, 0.24005, 0.26593, 0.25947, 0.20285, 0.16447))
+        r = codec.autocorr2refl_coeffs(autocorr)
+        assert_almost_equal(r, (-0.88066, -0.86178, -0.84593, -0.83941,
+            -0.85674, -0.89334, -0.93872, -0.98114, -0.99999, -0.99999),
+            decimal=4)
+
+# generated for sin440
+        autocorr = np.array((1.34078e+09, 1.25291e+09, 1.01818e+09, 6.66757e+08,
+            2.42955e+08, -2.01145e+08, -6.11889e+08, -9.40804e+08, -1.15071e+09,
+            -1.21930e+09, -1.14262e+09))
+        r = codec.autocorr2refl_coeffs(autocorr)
+        assert_almost_equal(r, (-0.93446, -0.87155, -0.80088, -0.71565,
+            -0.60413, -0.44283, -0.17919, 0.29794, 0.92724, 0.93862),
+            decimal=4)
+
+# total silence
+        autocorr = np.array((0., 0., 0., 0., 0., 0., 0., 0., 0., 0., 0.))
+        r = codec.autocorr2refl_coeffs(autocorr)
+        assert_almost_equal(r, (-0., -0., -0., -0., -0., -0., -0., -0., -0., -0.),
+                decimal=4)
+
+# silence with offset
+        autocorr = np.array((1.07321e+08, 1.06597e+08, 1.05789e+08, 1.04877e+08,
+            1.03885e+08, 1.02803e+08, 1.01634e+08, 1.00378e+08, 9.90601e+07,
+            9.76487e+07, 9.61770e+07))
+        r = codec.autocorr2refl_coeffs(autocorr)
+        assert_almost_equal(r, (-0.99325, -0.99281, -0.99234, -0.99187, -0.99140,
+            -0.99094, -0.99047, -0.99000, -0.98953, -0.98906), decimal=4)
+
 
 if __name__ == '__main__':
     unittest.main()
